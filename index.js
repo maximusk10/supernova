0412827ea474bb2806e322f47b35499f2599d867
max <proudmsk10@gmail.com>
2019-01-19 03:04:16 -0300

    misc: Testing some functionalities

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Load dependencies
 const express = require('express')
-
+const path = require('path')
+// Load .env
 require('dotenv').config()
-
+// Bootstrap app
 const app = express()
-
+// Config template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 app.get('/', (req, res)=> {
-    res.send('hello world')
+    res.render('index')
 })
 
 app.listen(process.env.PORT, () => {
